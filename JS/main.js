bb9c7b35417e43d821d86fcb31f3f3fb7cde6455
Tim Roks <h.p.t.roks@gmail.com>
2024-01-18 16:14:31 +0100

    dynamically added * to required labels

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,15 @@
-for(let i=0; i<5; i++){
-    console.log(i)
+let req_fields = document.getElementById("form").querySelectorAll("[required]");
+let req_labels = document.getElementsByTagName("label");
+let tempArr =[];
+
+req_fields.forEach(field =>{
+    // field.classList.add("required");
+    tempArr.push(field.id)
+})
+
+for(let i=0; i<req_labels.length; i++){
+    let tempVal = req_labels[i].attributes["for"].value;
+    if(tempArr.includes(tempVal)){
+        req_labels[i].classList.add("required")
+    }
 }
